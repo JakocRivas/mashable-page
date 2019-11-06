7780254b25f72fdd6ad525f3cd1e84d7dfe60b4e
Jakoc Rivas <jrivas@intellisys.com.do>
2019-11-06 15:32:07 -0400

    adds hamburger on mobile view

--- js/index.js
+++ js/index.js
@@ -113,26 +113,29 @@ Array.from(document.querySelectorAll(".article")).forEach(elem => {
     document.querySelector(`div.${id[1]}`).classList.remove("hidden");
   });
 });
-// const navSlide = () => {
-//   const burger = document.querySelector(".hamburger");
-//   const nav = document.querySelector(".menu-links");
-//   const navLinks = document.querySelectorAll(".menu-links li");
 
-//   // Toggle nav
-//   burger.addEventListener("click", () => {
-//     nav.classList.toggle("nav-active");
+/* HAMBURGER CONTROLLLER */
+const navSlide = () => {
+  const burger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".menu-links");
+  // ".menu-links li.title-menu:not(:first-child)"
+  const navLinks = document.querySelectorAll(".title-menu");
 
-//     // Animate links
-//     navLinks.forEach((link, index) => {
-//       if (link.style.animation) {
-//         link.style.animation = "";
-//       } else {
-//         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
-//           1.5}s`;
-//       }
-//     });
-//     // Burger animation
-//     burger.classList.toggle("toggle");
-//   });
-// };
-// navSlide();
+  // Toggle nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    // Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
+          1.5}s`;
+      }
+    });
+    // Burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+navSlide();
