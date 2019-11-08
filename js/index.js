a2f1cef3379f02ee9d0dbccc443529957fd17c58
Jakoc Rivas <jrivas@intellisys.com.do>
2019-11-08 14:44:51 -0400

    adds content to the page and some styling

--- js/index.js
+++ js/index.js
@@ -67,7 +67,60 @@ const controlMenu = () => {
     },
     {
       name: "SHOP",
-      subMenu: [{ title: "sumbo" }],
+      subMenu: [
+        {
+          title: "Tech",
+          subArticles: [
+            "VPN",
+            "Headphones",
+            "Speakers",
+            "Laptops",
+            "Web Hosting",
+            "Antivirus"
+          ]
+        },
+        {
+          title: "LifeStyle",
+          subArticles: ["Home", "Kitchen", "Gift Guides", "Gaming"]
+        },
+        {
+          title: "Culture",
+          subArticles: ["Dating", "Pets", "Subscription Boxes"]
+        },
+        {
+          title: "Best of Tech",
+          subArticles: [
+            "Best VPN",
+            "Best Cheap VPN",
+            "Best Streaming Services",
+            "Best Cheap Laptops",
+            "Best Running Headphones",
+            "Best Bluetooth Speakers"
+          ]
+        },
+        {
+          title: "Best of Culture",
+          subArticles: [
+            "Best Dating Sites",
+            "Best Free Dating Sites",
+            "Best Dating Sites for Introverts",
+            "Best DNA Tests",
+            "Best Dog DNA Tests",
+            "Best Subscription Boxes"
+          ]
+        },
+        {
+          title: "Best of LifeStyle",
+          subArticles: [
+            "Best Airfryer",
+            "Best Cordless Vacuum",
+            "Best Instant Pot",
+            "Best Gifts Under $50",
+            "Best Robot Vacuums",
+            "Best Vacuums for Pet Hair"
+          ]
+        }
+      ],
       type: "menu-more"
     },
     {
@@ -85,10 +138,43 @@ const controlMenu = () => {
           ]
         },
         {
-          title: "news",
-          subArticles: ["/pol/", "cnn", "fox news", "black pigeon"]
+          title: "Company",
+          subArticles: ["Licensing & Reprints", "archive", "Mashable careers"]
         },
-        { title: "sports", subArticles: ["baseball"] }
+        {
+          title: "Contact",
+          subArticles: ["Contact Us", "Submit News", "Mashable Shop"]
+        },
+        {
+          title: "Legal",
+          subArticles: [
+            "Privacy Policy",
+            "Term of Use",
+            "Cookie Policy",
+            "Accessibility Statement"
+          ]
+        },
+        {
+          title: "Resources",
+          subArticles: [
+            "Travel",
+            "Security",
+            "How To",
+            "Mashable Deals",
+            "Gift Guides"
+          ]
+        },
+        { title: "Sites", subArticles: ["Job Board", "Social Good Summit"] },
+        {
+          title: "International",
+          subArticles: [
+            "Mashable Australia",
+            "Mashable India",
+            "Mashable ME",
+            "Mashable SE Asia",
+            "Mashable UK"
+          ]
+        }
       ],
       type: "menu-more"
     }
