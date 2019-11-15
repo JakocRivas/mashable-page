053653ccb3b826f40185c1dde5048c88f28a5dfd
Jakoc Rivas <jrivas@intellisys.com.do>
2019-11-15 10:27:53 -0400

    fixes indentation errors with the html inside the backticks

--- js/views/menuView.js
+++ js/views/menuView.js
@@ -10,7 +10,10 @@ export const createMenu = ({ name, subMenu, type, id }) => {
 
   function createArticleLI(submenu) {
     const map = submenu.subArticles.map(subMenuLI => {
-      return `<li class="menu__more-sub-article"><a class="title-label">${subMenuLI}</a></li>`;
+      return `<li class="menu__more-sub-article">
+                  <a class="title-label">${subMenuLI}</a>
+              </li>
+              `;
     });
     return map.join().replace(/,/g, "");
   }
@@ -24,7 +27,11 @@ export const createMenu = ({ name, subMenu, type, id }) => {
         article += createArticleLI(element, id);
       }
 
-      column += `<ul class="menu__more-articles"><li class="title-menu-more">${element.title}</li>${article}</ul>`;
+      column += ` 
+                <ul class="menu__more-articles">
+                    <li class="title-menu-more">${element.title}</li>${article}
+                </ul>
+                `;
     });
   }
 
@@ -37,29 +44,38 @@ export const createMenu = ({ name, subMenu, type, id }) => {
       article += createArticleLI(elem, id);
     }
 
-    subArticle += `<div class="subArticles ${newID}"><ul class="article-post">${article}</ul></div>`;
-    menu += `<li class="article ${newID}"><a class="article-font">${elem.title}</a></li>`;
+    subArticle += `
+                  <div class="subArticles ${newID}">
+                      <ul class="article-post">${article}</ul>
+                  </div>
+                  `;
+
+    menu += `
+            <li class="article ${newID}">
+                <a class="article-font">${elem.title}</a>
+            </li>
+            `;
   });
   return `
-    <li class="title-menu">
-      <a class="title-name ${id}">${name}</a>
-        ${
-          subMenu.length > 0
-            ? type !== "menu-more"
-              ? `<div class='submenu-container'><div><ul class='${className}'>${menu}</ul></div><div class="sub-articles-container">${subArticle}</div></div>`
-              : `<div class='submenu-container'><ul class='${className}'>${column}</ul></div>`
-            : ""
-        }
-    </li>
+        <li class="title-menu">
+          <a class="title-name ${id}">${name}</a>
+            ${
+              subMenu.length > 0
+                ? type !== "menu-more"
+                  ? `<div class='submenu-container'><div><ul class='${className}'>${menu}</ul></div><div class="sub-articles-container">${subArticle}</div></div>`
+                  : `<div class='submenu-container'><ul class='${className}'>${column}</ul></div>`
+                : ""
+            }
+        </li>
   `;
 };
 
 export const createSocial = ({ name }) => {
   return `
-  <li class="title-social">
-    <a class="title-name">${name}</a>
-  </li>
-`;
+        <li class="title-social">
+            <a class="title-name">${name}</a>
+        </li>
+      `;
 };
 
 export const renderMenus = menu => {
