0db20ea59f4a7a7d55bddf6c58291d2fa5abb179
Jakoc Rivas <jrivas@intellisys.com.do>
2019-11-25 08:40:15 -0400

    reset

--- js/views/base.js
+++
@@ -1,6 +0,0 @@
-const elements = {
-  menuContainer: document.querySelector(".menu-links"),
-  subMenuContainer: document.querySelector(".article-post"),
-  socialContainer: document.querySelector(".social-links")
-};
-export default elements;
