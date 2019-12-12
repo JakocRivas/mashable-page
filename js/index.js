bcc0580b20c84c6e83149485039d7896277feb09
Jakoc Rivas <jrivas@intellisys.com.do>
2019-12-12 11:57:23 -0400

    change the if else statement on the toogle nav animation style to a ternary operator

--- js/index.js
+++ js/index.js
@@ -93,13 +93,9 @@ const navSlide = async () => {
 
     // Animate links
     navLinks.forEach((link, index) => {
-      console.log(link);
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
-          1.5}s`;
-      }
+      link.style.animation = link.style.animation
+        ? ""
+        : `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
     });
 
     // Burger animation
